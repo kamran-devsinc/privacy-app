3b4c6420d1aab3d411e76bb4ddcddc01e3a03850
Kamran Arshad <kamran.arshad@devsinc.com>
2021-06-10 04:22:16 +0500

    Updated all users api

--- controllers/users.js
+++ controllers/users.js
@@ -14,7 +14,10 @@ const getUserProfile = async (req, res) => {
 
 const getAllUsers = async (req, res) => {
   try {
-    const users = await User.find({}).select('_id name')
+    const users = await User.find({ _id: { $ne: req.user._id } }).populate({
+      path:'connections.value',
+      match: { 'userId': { $in: [req.user._id] } }
+    })
 
     return res.status(200).json({ users })
   } catch (err) {
